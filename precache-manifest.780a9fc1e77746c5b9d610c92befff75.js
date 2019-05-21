250b66cbda6df9e8f83278be0601c2c847c14657
Alex Wilkinson <awilk901@gmail.com>
2019-05-21 07:44:18 -0500

    Updates

---
+++ precache-manifest.780a9fc1e77746c5b9d610c92befff75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4bcaa2099f6e2b5222894111bbd6266",
+    "url": "/roll-dice/index.html"
+  },
+  {
+    "revision": "3ae771067b51ad6bfff6",
+    "url": "/roll-dice/static/css/main.27e33d38.chunk.css"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "/roll-dice/static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "3ae771067b51ad6bfff6",
+    "url": "/roll-dice/static/js/main.471dce98.chunk.js"
+  },
+  {
+    "revision": "695159fe7b55d929c112",
+    "url": "/roll-dice/static/js/runtime~main.a30d1dd8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/roll-dice/static/media/logo.5d5d9eef.svg"
+  }
+]);
